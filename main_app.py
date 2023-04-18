42f6b0cb70603482ad91bccd6bd680b9ea94bbc9
Mateo Escamilla <91042644+2ie@users.noreply.github.com>
2023-04-18 17:54:29 -0500

    Initial commits

--- main_app.py
+++ main_app.py
@@ -1,2 +1,9 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def registration():
+    return render_template("registration.html")
